301391ca8a2b39109141501ad049c79c0da5fdf7
akashwebs <aksah.shil227@gmail.com>
2022-09-06 21:38:27 +0600

    added pagination

--- src/Pages/Dashboard/AllFamilyMember.js
+++ src/Pages/Dashboard/AllFamilyMember.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Heading from "../../Shared/Heading";
 import Loading from "../../Shared/Loading";
 import FamilyMemberRow from "./FamilyMemberRow";
 import { useQuery } from "@tanstack/react-query";
 import swal from "sweetalert";
+import ReactPaginate from "react-paginate";
 
 const AllFamilyMember = () => {
   const handleDeleteMember = (id) => {
@@ -43,6 +44,22 @@ const AllFamilyMember = () => {
   } = useQuery(["allMemberProfile"], () =>
     fetch(url).then((res) => res.json())
   );
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 15;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(members?.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(members?.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, members]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % members?.length;
+    setItemOffset(newOffset);
+  };
 
   if (isLoading) {
     return <Loading></Loading>;
@@ -74,8 +91,29 @@ const AllFamilyMember = () => {
             ))}
           </tbody>
 
-          <tfoot>
-            <tr>{/* herer pagination */}</tr>
+          <tfoot className="">
+            <tr className="">
+              {/* paginate */}
+              <td colSpan={5} className="">
+                <div className="">
+                  <ReactPaginate
+                    breakLabel="..."
+                    nextLabel=">>"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={1}
+                    marginPagesDisplayed={1}
+                    pageCount={pageCount}
+                    previousLabel="<<"
+                    renderOnZeroPageCount={null}
+                    containerClassName="btn-group pagination"
+                    pageLinkClassName="btn btn-sm"
+                    previousLinkClassName="btn-sm btn"
+                    nextLinkClassName="btn btn-sm"
+                    activeClassName="pagination-active"
+                  />
+                </div>
+              </td>
+            </tr>
           </tfoot>
         </table>
       </div>
